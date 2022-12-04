96376b9f1d2e867b2a0e7d9faa91bd404e80de15
Databob42 <118942380+Databob42@users.noreply.github.com>
2022-12-04 11:00:56 -0700

    Update streamlit_app.py
    add fruit_choice variable and associated text entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,11 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruity vice api response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # remove this line from the app to clear raw text from screen:  streamlit.text(fruityvice_response.json())  #This just writes the data to the screen
 
 #Take the json version and normalize it
