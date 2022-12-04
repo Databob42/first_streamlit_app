a435b5df5e3bc8ecf4aa9b34d53076eb0e7a856b
Databob42 <118942380+Databob42@users.noreply.github.com>
2022-12-04 10:43:05 -0700

    Update streamlit_app.py
    removed raw text from application screen

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())  #This just writes the data to the screen
+# remove this line from the app to clear raw text from screen  streamlit.text(fruityvice_response.json())  #This just writes the data to the screen
 
 #Take the json version and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
